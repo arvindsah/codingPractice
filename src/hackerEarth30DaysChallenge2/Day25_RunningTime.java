53a2e445617efbf4c6e32710b092f86bbaa9fb1a
Arvind Sah <arvindsah005@gmail.com>
2021-08-21 00:12:50 +0530

    day 25 time complexity

--- src/hackerEarth30DaysChallenge2/Day25_RunningTime.java
+++ src/hackerEarth30DaysChallenge2/Day25_RunningTime.java
@@ -2,27 +2,40 @@ package hackerEarth30DaysChallenge2;
 
 import java.util.Scanner;
 
+
 public class Day25_RunningTime {
 
 	 public static void main(String[] args) {
-	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	        /* Enter your code here. Read input from STDIN. 
+	         * Print output to STDOUT. Your class should be named Solution. */
 		 
 		 Scanner scan= new Scanner(System.in);
-		 int size=scan.nextInt();
-		 
-		 int[] arr= new int[size];
-		 for(int i=0;i<size;i++) {
-			 arr[i]=scan.nextInt();
-		 }
-		 
-		 for(int i=2;i<arr[size-1];i++) {
-			 if(arr[i]%i==0)
-{
-	System.out.println("Not Prime");
-}
-}
-		 }
-	    }
-	}
+         int size=scan.nextInt();
+         
+         int[] arr= new int[size];
+         for(int j=0;j<size;j++) {
+             arr[j]=scan.nextInt();
+             
+             if(arr[j]==1)
+                 {System.out.println("Not prime");
+                 continue;}
+             boolean isPrime=true;
+             
+             for (int i = 2; i * i <= arr[j]; i++) {
+                    if (arr[j] % i == 0) {
+                    isPrime=false;
+                }
+            }
+             if(isPrime) {
+                 System.out.println("Prime");
+             }else {
+                 System.out.println("Not prime");
+             }
+             
+         }
+        
+         scan.close();
+        }
+	 }
 
 
