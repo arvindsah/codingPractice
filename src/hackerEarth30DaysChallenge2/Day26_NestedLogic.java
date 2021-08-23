9c976300a076b6f086746ce768d9ec5b21006ed3
Arvind Sah <arvindsah005@gmail.com>
2021-08-23 23:23:27 +0530

    day 27 Testing

--- src/hackerEarth30DaysChallenge2/Day26_NestedLogic.java
+++ src/hackerEarth30DaysChallenge2/Day26_NestedLogic.java
@@ -8,37 +8,40 @@ package hackerEarth30DaysChallenge2;
 	    public static void main(String[] args) {
 	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 	    
-	    
-	int fine;
-	        
-	        Scanner scan = new Scanner(System.in);
-	        
-	        int returnDay = scan.nextInt();
-	        int returnMonth = scan.nextInt();
-	        int returnYear = scan.nextInt();
-	        
-	        int dueDay = scan.nextInt();
-	        int dueMonth = scan.nextInt();
-	        int dueYear = scan.nextInt();
-	        
-	        scan.close();
-	        
-	        if(returnYear <= dueYear){
-	            if(returnMonth <= dueMonth){
-	                if(returnDay <= dueDay){
-	                    fine = 0;
-	                }else{
-	                    fine = (returnDay - dueDay) * 15;
-	                }
-	            }else{
-	                fine = (returnMonth - dueMonth) * 500;
-	            }
-	        }else{
-	            fine = 10000;
-	        }
-	        
-	        System.out.println(fine);
-	    }
-	    
-	}
-	    
+
+int fine=0;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int returnDay = scan.nextInt();
+        int returnMonth = scan.nextInt();
+        int returnYear = scan.nextInt();
+        
+        int dueDay = scan.nextInt();
+        int dueMonth = scan.nextInt();
+        int dueYear = scan.nextInt();
+        
+        scan.close();
+        
+        if(returnYear == dueYear){
+            if(returnMonth <= dueMonth){
+                if(returnDay <= dueDay){
+                    fine = 0;
+                }else{
+                    fine = (returnDay - dueDay) * 15;
+                }
+            }else{
+                fine = (returnMonth - dueMonth) * 500;
+            }
+        } else if(returnYear < dueYear){
+            fine=0;
+        }
+        else{
+            fine = 10000;
+        }
+        
+        System.out.println(fine);
+    }
+    
+}
+    
